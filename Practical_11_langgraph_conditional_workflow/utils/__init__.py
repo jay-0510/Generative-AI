2308810a8a3923ec13f141a20b863b7f20f997c0
Patel Jay <pateljay51003@gmail.com>
2026-07-28 21:29:17 +0530

    feat(utils): implement shared workflow helpers and package init

---
+++ Practical_11_langgraph_conditional_workflow/utils/__init__.py
@@ -0,0 +1,7 @@
+"""
+utils package
+-------------
+Small, generic, reusable helper functions that don't belong to any one
+agent -- currently just the safe SQL execution guard used by the SQL
+agent path (see helpers.py).
+"""
